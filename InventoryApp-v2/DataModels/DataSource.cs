cd80fe46bb08ec1806fd5abf30582bc25f5f5380
Erich Dingeldein <edingeldien@psdoors.com>
2018-11-14 15:58:06 -0600

    Initial Commit

---
+++ InventoryApp-v2/DataModels/DataSource.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Util;
+using Android.Views;
+using Android.Widget;
+using Java.Lang;
+using Application = Android.App.Application;
+using Exception = System.Exception;
+using File = System.IO.File;
+
+namespace InventoryApp_v2.DataModels
+{
+    public class DataSource
+    {
+        private const string _webApiUri = "http://mfg-fs:5482/InventoryService/";
+        private const string _serializedFileName = "lastMasterList.json";
+        private readonly Dictionary<string, string> _commands = new Dictionary<string, string>()
+        {
+            { "PingSource", "PingDevice" },
+            { "GetMasterList", "/GetData/RackSummary"},
+            { "GetParentList", "/GetData/ParentParts" },
+            { "PostSummary", "/PostData/summary.json" }
+        };
+
+        public async Task<bool> PingSource()
+        {
+            try
+            {
+                var request = WebRequest.Create(_webApiUri + _commands["PingSource"]) as HttpWebRequest;
+                request.Method = "GET";
+                request.KeepAlive = false;
+
+                var pingSource = Task.Run(() =>
+                {
+                    using (var response = request.GetResponse() as HttpWebResponse)
+                    using (var responseStream = new StreamReader(response.GetResponseStream()))
+                    {
+                        var result = responseStream.ReadToEnd();
+                        Log.Debug("InventoryApp.DataSource", "Response from service: " + result);
+                        return result.Equals("true");
+                    }
+                });
+
+                return await pingSource;
+            }
+            catch (Exception e)
+            {
+                Log.Error("InventoryApp.DataSource", "Error pinging source:\t" + e.Message);
+                return false;
+            }
+        }
+
+        public async Task<string> GetMasterListFromWeb()
+        {
+            var request = WebRequest.Create(_webApiUri + _commands["GetMasterList"]) as HttpWebRequest;
+            request.Method = "GET";
+            request.KeepAlive = false;
+            request.Timeout = 12000;
+
+            try
+            {
+                using (var response = await request.GetResponseAsync())
+                using (var responseStream = new StreamReader(response.GetResponseStream()))
+                {
+                    var content = responseStream.ReadToEnd();
+                    return content;
+                }
+            }
+            catch (Exception e)
+            {
+                Log.Error("InventoryApp.DataSource", "Error getting master list:\t" + e.Message);
+                return null;
+            }
+        }
+
+        public async Task<string> GetMasterListFromDisk()
+        {
+            var baseDir = Application.Context.FilesDir.AbsolutePath;
+            var filePath = Path.Combine(baseDir, _serializedFileName);
+
+            try
+            {
+                var readFile = Task.Run(() => File.ReadAllText(filePath));
+                return await readFile;
+            }
+            catch (Exception e)
+            {
+                Log.Error("InventoryApp.DataSource", "Error reading master list from file: " + e.Message);
+                return null;
+            }
+
+        }
+
+        public async Task WriteMasterListToDisk(string serializedMasterList)
+        {
+            var baseDir = Application.Context.FilesDir.AbsolutePath;
+            var savePath = Path.Combine(baseDir, _serializedFileName);
+
+            try
+            {
+                var writeFile = Task.Run(() => File.WriteAllText(savePath, serializedMasterList));
+                await writeFile;
+            }
+            catch (Exception e)
+            {
+                Log.Error("InventoryApp.DataSource", "Error writing master list to disk: " + e.Message);
+            }
+        }
+
+    }
+}
---
+++ InventoryApp-v2/Views/MainView.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+using Android.App;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using InventoryApp_v2.ViewModels;
+using ReactiveUI;
+
+namespace InventoryApp_v2.Views
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainView : Activity, IViewFor<MainViewModel>
+    {
+        object IViewFor.ViewModel
+        {
+            get => ViewModel;
+            set => ViewModel = (MainViewModel)value;
+        }
+        public MainViewModel ViewModel { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.main_view);
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            // TODO subscribe to view model data
+        }
+
+        protected override void OnPause()
+        {
+            base.OnPause();
+            // TODO unsubscribe from view model data
+        }
+
+
+    }
+}
+
